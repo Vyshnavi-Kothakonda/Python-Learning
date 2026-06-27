dbfb60e361c3b7eb0a4fd8698a338d66becd999f
Vyshnavi Kothakonda <vyshnavikothakonda14@gmail.com>
2026-06-27 22:10:35 +0530

    Add ToggleCase.py to toggle letter cases

---
+++ Strings/ToggleCase.py
@@ -0,0 +1,10 @@
+s = input()
+result = ""
+for ch in s:
+    if ch.islower():
+        result += ch.upper()
+    elif ch.isupper():
+        result += ch.lower()
+    else:
+        result += ch
+print(result)
