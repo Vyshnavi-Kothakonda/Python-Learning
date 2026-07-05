3fdadd735e2e896105e1a807e08d2214ac8db206
Vyshnavi Kothakonda <vyshnavikothakonda14@gmail.com>
2026-07-05 22:35:33 +0530

    Add binary search implementation in BinarySearch.py

---
+++ Lists/BinarySearch.py
@@ -0,0 +1,15 @@
+nums = list(map(int, input().split()))
+target = int(input())
+left = 0
+right = len(nums) - 1
+while left <= right:
+    mid = (left + right) // 2
+    if nums[mid] == target:
+        print("Element found at index:", mid)
+        break
+    elif nums[mid] < target:
+        left = mid + 1
+    else:
+        right = mid - 1
+else:
+    print("Element not found")
