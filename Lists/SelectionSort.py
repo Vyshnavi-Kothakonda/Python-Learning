97824eafc9f01e7e3b6d8290c990fb76d66132ed
Vyshnavi Kothakonda <vyshnavikothakonda14@gmail.com>
2026-07-06 20:36:34 +0530

    Implement Selection Sort algorithm in Python

---
+++ Lists/SelectionSort.py
@@ -0,0 +1,8 @@
+nums = list(map(int, input().split()))
+for i in range(len(nums)):
+    min_index = i
+    for j in range(i + 1, len(nums)):
+        if nums[j] < nums[min_index]:
+            min_index = j
+    nums[i], nums[min_index] = nums[min_index], nums[i]
+print("Sorted List:", nums)
