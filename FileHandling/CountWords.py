902dccc10d1e9e30e48f61bcc532d88ed12997ad
Vyshnavi Kothakonda <vyshnavikothakonda14@gmail.com>
2026-07-15 17:54:31 +0530

    Add word counting functionality to CountWords.py

---
+++ FileHandling/CountWords.py
@@ -0,0 +1,8 @@
+filename = input("Enter file name: ")
+try:
+    with open(filename, "r") as file:
+        text = file.read()
+        words = text.split()
+        print("Number of words:", len(words))
+except FileNotFoundError:
+    print("File not found.")
