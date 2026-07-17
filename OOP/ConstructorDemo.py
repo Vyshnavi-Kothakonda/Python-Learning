0324d0f1243675427283358187f6eb5b6c4d4ad2
Vyshnavi Kothakonda <vyshnavikothakonda14@gmail.com>
2026-07-17 19:42:13 +0530

    Add Student class with constructor and display method

---
+++ OOP/ConstructorDemo.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self):
+        self.name = "Vyshnavi"
+        self.age = 19
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+s1 = Student()
+s1.display()
