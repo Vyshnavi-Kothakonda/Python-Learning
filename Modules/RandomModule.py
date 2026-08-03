df4ede5b6aafd72cc2dec7f236477a0453459048
Vyshnavi Kothakonda <vyshnavikothakonda14@gmail.com>
2026-08-03 22:42:31 +0530

    Add RandomModule.py for random number operations

---
+++ Modules/RandomModule.py
@@ -0,0 +1,6 @@
+import random
+numbers = [10, 20, 30, 40, 50]
+print("Random Number:", random.randint(1, 100))
+print("Random Choice:", random.choice(numbers))
+random.shuffle(numbers)
+print("Shuffled List:", numbers)
