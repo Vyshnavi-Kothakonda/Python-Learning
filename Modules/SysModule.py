2a027145c5f9a64dc4c9c3d762a3b796f8488261
Vyshnavi Kothakonda <vyshnavikothakonda14@gmail.com>
2026-08-06 22:18:06 +0530

    Add SysModule.py to display system information

---
+++ Modules/SysModule.py
@@ -0,0 +1,9 @@
+import sys
+print("Python Version:")
+print(sys.version)
+print("\nPlatform:")
+print(sys.platform)
+print("\nMaximum Integer Size:")
+print(sys.maxsize)
+print("\nCommand Line Arguments:")
+print(sys.argv)
