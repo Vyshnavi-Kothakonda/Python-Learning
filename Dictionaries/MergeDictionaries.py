6326565408e62aab527193fe8cedbec478346315
Vyshnavi Kothakonda <vyshnavikothakonda14@gmail.com>
2026-08-25 22:09:24 +0530

    Add MergeDictionaries.py to merge two dictionaries

---
+++ Dictionaries/MergeDictionaries.py
@@ -0,0 +1,11 @@
+dict1 = {
+    "name": "Vyshnavi",
+    "course": "CSD"
+}
+dict2 = {
+    "college": "SWEC",
+    "year": 2
+}
+merged = {**dict1, **dict2}
+print("Merged Dictionary:")
+print(merged)
